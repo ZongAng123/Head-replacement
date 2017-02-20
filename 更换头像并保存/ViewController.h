2c86953af04a8a9e9b7734e53715e51e8a5132f2
ZongAng123 <zongang15@yahoo.com>
2017-02-20 15:56:46 +0800

    Headreplacement
    Head replacement

--- 更换头像并保存/ViewController.h
+++ 更换头像并保存/ViewController.h
@@ -7,9 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
-
+//获取系统版本号
+#define IS_iOS8 ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0 ? YES : NO)
+//获取屏幕尺寸
+#define SW [UIScreen mainScreen].bounds.size.width
+#define SH [UIScreen mainScreen].bounds.size.height
+@interface ViewController : UIViewController<UINavigationControllerDelegate,UIImagePickerControllerDelegate,UIActionSheetDelegate>
+@property (nonatomic, strong)UIButton *iconBtn;
 
 @end
 
